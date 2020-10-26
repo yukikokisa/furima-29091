cd0a4ed01ae51a21ddc3537ad72b7d3056a9f831
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2020-10-26 20:28:54 +0900

    新規登録の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :last_name, :fast_name, :last_name_kana, :fast_name_kana, :birth_date ,presence: true
+  has_many :items
 end
