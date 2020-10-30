bd7401b68876ff19d71aa0a7ed81c56d827d133c
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2020-10-30 21:41:23 +0900

    Active Storageの導入等

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,14 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
