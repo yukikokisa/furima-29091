a9144749a3458f5fd9ea81880736a6186e163532
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2020-10-30 18:22:39 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items  
-      
-  validates :nickname,        presence: true, length: { maximum: 40 }
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :fast_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :fast_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date ,     presence: true
+  with_options presence: true do
+    validates :nickname,         length: { maximum: 40 }
+    validates :last_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :fast_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :fast_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_date 
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-      it "nicknameが40文字以上であれば登録できない" do
+     it "nicknameが40文字以上であれば登録できない" do
         @user.nickname = "a" * 41
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname is too long (maximum is 40 characters)")
@@ -35,16 +35,27 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
+      it "emailは@を含んだ半角英数字で入力されていない場合登録できない" do
+        @user.email = "おおお.gmail.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it "passwordが空では登録できない" do
         @user.password = ""
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it "passwordが5文字以下であれば登録できない" do
+      it "passwordが5文字以下であると登録できない" do
         @user.password = "00000"
         @user.password_confirmation = "00000"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it "passwordが半角英数字混合で入力されていない場合登録できない" do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include()
       end
       it "passwordが存在してもpassword_confirmationが空では登録できない" do
         @user.password_confirmation = ""
@@ -56,20 +67,40 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
+      it "last_nameが半角英数字では登録できない" do
+        @user.last_name = "sasaki"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
       it "fast_nameが空では登録できない" do
         @user.fast_name = ""
         @user.valid?
         expect(@user.errors.full_messages).to include("Fast name can't be blank")
+      end
+      it "fast_nameが半角英数字では登録できない" do
+        @user.fast_name = "sasaki"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Fast name is invalid")
       end
       it "last_name_kanaが空では登録できない" do
         @user.last_name_kana = ""
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name kana can't be blank")
       end
+      it "last_name_kanaが半角英数字では登録できない" do
+        @user.last_name_kana = "ｽｽﾞｷ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
       it "fast_name_kanaが空では登録できない" do
         @user.fast_name_kana = ""
         @user.valid?
         expect(@user.errors.full_messages).to include("Fast name kana can't be blank")
+      end
+      it "fast_name_kanaが半角英数字では登録できない" do
+        @user.fast_name_kana = "ｽｽﾞｷ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Fast name kana is invalid")
       end
       it "birth_dateが空では登録できない" do
         @user.birth_date = ""
@@ -78,4 +109,4 @@ RSpec.describe User, type: :model do
       end
     end
   end
-end
+end 
