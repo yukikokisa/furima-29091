4e07607f9aa480d0ad8c8989b1822e7720500732
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2020-11-05 13:08:19 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,18 +10,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_date
 
   with_options presence: true do
-    validates :image
+    validates :image, :category, :condition, :postage_player, :shipping_area, :shipping_date
     validates :name,              length: { maximum: 40 }
     validates :description,       length: { maximum: 1000 }
-    validates :category
     validates :category_id, numericality: { other_than: 1 }
-    validates :condition
     validates :condition_id, numericality: { other_than: 1 }
-    validates :postage_player
     validates :postage_player_id, numericality: { other_than: 1 }
-    validates :shipping_area
     validates :shipping_area_id, numericality: { other_than: 1 }
-    validates :shipping_date
     validates :shipping_date_id,  numericality: { other_than: 1 }
     validates :price,             format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -64,12 +64,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceが、300以下では登録できない' do
-        @item.price = '100'
+        @item.price = 100
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが、9999999以上では登録できない' do
-        @item.price = '99999999'
+        @item.price = 99999999
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
