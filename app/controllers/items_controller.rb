a7e333c00faf31d8c85de85b6daf8ca745896772
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2020-11-07 17:13:48 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :set_message, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
@@ -19,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -39,5 +37,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_player_id, :shipping_area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_message
+    @item = Item.find(params[:id])
   end
 end
